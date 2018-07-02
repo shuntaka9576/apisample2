1a4ec1a75233505cfdbe25e5c3051c82a12c49a1
shutaka9576 <shuntaka9576@gmail.com>
2018-07-02 22:54:45 +0900

    DBのモックを使ってAPI処理を記述

--- nethttp/OK, We Need to Split This Up/handlers.go
+++ nethttp/OK, We Need to Split This Up/handlers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"strconv"
 )
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -25,5 +26,28 @@ func TodoIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 }
 
 func TodoShow(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "Todo show: %s", ps.ByName("todoId"))
+	idParam := ps.ByName("todoId")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422)
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	t := RepoFindTodo(id)
+	if t.ID == 0 && t.Name == "" {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(t); err != nil {
+		panic(err)
+	}
+	return
 }
